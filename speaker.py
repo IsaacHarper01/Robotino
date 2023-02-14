6e9aa1960fa2a492f6de8ee2b65c7a401ba53e75
IsaacHarper01 <isaac.anibal.pk@gmail.com>
2023-02-13 18:39:01 -0600

    speaker node ready

--- speaker.py
+++ speaker.py
@@ -5,14 +5,17 @@ from std_msgs.msg import String
 
 class speaker_ros:
     def __init__(self):
-        self.engine = pyttsx3.init() 
-        #self.rate = engine.getProperty('rate')   # getting details of current speaking rate
-        self.engine.setProperty('rate', 180)
+        self.engine = pyttsx3.init()
+        voices = self.engine.getProperty('voices') 
+        self.engine.setProperty('voice', voices[2].id)
+        self.engine.setProperty('rate', 150)
         self.sub = rospy.Subscriber("/speak", String, self.speakcall)
 
     def speakcall(self, text):
-        self.engine.say(text)
+        self.engine.say(text.data)
         self.engine.runAndWait()
         self.engine.stop()
 
+rospy.init_node("speaker_node")
 speaker = speaker_ros()
+rospy.spin()
